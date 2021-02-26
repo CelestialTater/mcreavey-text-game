25ed411d4e4ca626925049c8e01cf6cf89f0126d
Cole Dewis <46581209+CelestialTater@users.noreply.github.com>
2021-02-26 13:24:51 -0700

    Intro Text + Console Colors

--- src/com/coled/Main.java
+++ src/com/coled/Main.java
@@ -1,9 +1,23 @@
 package com.coled;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello!");
+        try (BufferedReader br = new BufferedReader(new FileReader("src/com/coled/intro.txt"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(Colors.RED + line + Colors.RESET);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
